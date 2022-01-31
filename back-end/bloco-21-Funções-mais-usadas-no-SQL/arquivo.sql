58401485139532cdad59d10cbd4bf48601070bb8
Adriana <adrianam754@gmail.com>
2022-01-31 16:12:51 -0300

    Exercicio de fixaçao 3

--- back-end/bloco-21-Funções-mais-usadas-no-SQL/arquivo.sql
+++ back-end/bloco-21-Funções-mais-usadas-no-SQL/arquivo.sql
@@ -1,6 +1,12 @@
 -- 1) Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
 SELECT UCASE('trybe');
 
+
 -- 2) Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .
 
-SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google')
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+
+
+-- Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .
+
+SELECT CHAR_LENGTH('Uma frase qualquer');
